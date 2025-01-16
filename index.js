f1ab129db85c07efb63af40a2e00f4ae62d75a4b
Kimeme <133958988+Kimeme@users.noreply.github.com>
2025-01-16 20:03:00 +0300

    add cors frontend

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ dotenv.config();
 
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 const app = express();
-app.use(cors({ origin: true }));
+app.use(
+  cors({
+     origin: ["https://amazon-frontend-website.netlify.app"]
+     })
+    );
+       // Replace with your frontend URL
+
 
 app.use(express.json());
 
